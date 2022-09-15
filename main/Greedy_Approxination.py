91fba2f71c97e3ab09de551d9e88ab71a3c35487
Ekaterina Milyutina <milyutie@nswi177.gitlab.mff.cuni.cz>
2022-09-15 15:04:17 +0200

    Greedy_Approximation: update

--- main/Greedy_Approxination.py
+++ main/Greedy_Approxination.py
@@ -4,15 +4,38 @@ from re import sub
 import sys
 from string_functions import *
 
-#function that find the shortest superstring 
-def findSuperStr (arr):
-    
-    first_str = arr.pop()
-    while len(arr) > 0:
-        second_str = arr.pop()
-        [prefix, overlap, suffix, resStr] = betterOverlap(first_str, second_str) #found overlap 
-        result = prefix + overlap + suffix #new string 
-        first_str = result
-    
-    return first_str 
- 
+
+def findSuperStr(arr):
+
+    leng = len(arr)
+
+    while leng != 1:
+        Overlap = -sys.maxsize - 1  #max overlap
+        #index of strings that are in overlap
+        first = 0
+        second = 0
+        resultStr = ""  #the resulting string
+
+        for i in range(0, leng):
+            for j in range(i + 1, leng):
+                [prefix, over, suffix, maxOver
+                 ] = overlap(arr[i],
+                             arr[j])  #get max overlap of two strings from arr
+                result = prefix + over + suffix
+
+                if (Overlap < maxOver):  #check for there is new max overlap
+                    Overlap = maxOver  #new max overlap
+                    resultStr = result  #new resulting string
+                    first = i  #first string in overlap is stored
+                    second = j  #second string in overlap is stored
+
+        leng = leng - 1  #last string in array will be ignored
+
+        #if there are no overlaps
+        if (Overlap == -sys.maxsize - 1):
+            arr[0] = arr[0] + arr[leng]
+        else:
+            arr[first] = resultStr  #first string is now new string
+            arr[second] = arr[leng]  #second string will be ignored
+
+    return arr[0]
