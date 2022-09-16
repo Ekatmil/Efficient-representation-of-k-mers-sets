5d0a193d7fea120a4439d026bfb7eb0a37ef7ffa
Ekaterina Milyutina <milyutie@nswi177.gitlab.mff.cuni.cz>
2022-09-16 15:16:15 +0200

    Mask Updated

--- main/mask.py
+++ main/mask.py
@@ -1,10 +1,7 @@
-from curses.ascii import isalpha
-
-
 def findMask(Kset, str1, k):
     l1 = list(str1)
     for i in range(0, len(l1) - k + 1):
-        substr = ''.join(map(str, l1[i:(i + k)]))
+        substr = str1[i:i + k]
         if substr in Kset:
             l1[i] = l1[i].lower()
     StrMask = ''.join(map(str, l1))
@@ -12,13 +9,12 @@ def findMask(Kset, str1, k):
 
 
 def findMaskBinary(Kset, str1, k):
-    l1 = list(str1)
-    for i in range(0, len(l1) - k + 1):
-        substr = ''.join(map(str, l1[i:(i + k)]))
+    l1 = []
+    for i in range(0, len(str1) - k + 1):
+        substr = str1[i:i + k]
         if substr in Kset:
-            l1[i] = "1"
-    for i in range(0, len(l1)):
-        if l1[i].isalpha():
-            l1[i] = "0"
+            l1.append("1")
+        else:
+            l1.append("0")
     StrMask = ''.join(map(str, l1))
     return StrMask
