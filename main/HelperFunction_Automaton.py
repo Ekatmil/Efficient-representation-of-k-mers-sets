0f7397cf3183b1b16b434ff4c6f784e1ddf5957c
Ekaterina Milyutina <milyutie@nswi177.gitlab.mff.cuni.cz>
2022-11-17 18:56:45 +0100

    HelperFunction_Automatopn.py: addition of function which allows all the strings to be in the path H

--- main/HelperFunction_Automaton.py
+++ main/HelperFunction_Automaton.py
@@ -23,3 +23,33 @@ def initializeForbidden (dict, m):
     for j in range (0, m):
         dict[j] = False
     return dict 
+
+
+#Function that adds to path additional non used words
+def AddingAdditionalStr (H, forbidden):
+    from_list = []
+    to_list = []
+    for ind, bo in forbidden.items():
+        print ("Index is: ", ind)
+        print ("Boolean is: ", bo)
+        if bo == False:
+            if H.get(ind) == None: #exist
+                from_list.append(ind)
+            to_list.append(ind)
+    print ("*To List is: ", to_list)
+    print ("*From list is: ", from_list)
+    while len(from_list) != 0:
+        print ("Length of from list is: ", len(from_list))
+        for i in to_list:
+            print ("I is: ", i)
+            if i not in from_list:
+                H[from_list[0]] = i
+                print ("New H is: ", H)
+                from_list.remove(from_list[0])
+                to_list.remove(i)
+                ("New From list is: ", from_list)
+                ("New To list is: ", to_list)
+    #NOTE: check this out. If necessary and if enough 
+    if len(to_list) >= 2:
+        H[to_list[0]] = to_list[1]
+    return H
