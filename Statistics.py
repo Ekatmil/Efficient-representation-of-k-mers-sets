e29819ba92adb3d4728c5b700d0b6f468f4e874a
Katya <ekatmil@gmail.com>
2023-02-22 15:13:02 +0100

    Add CPU time to statistics.py

--- Statistics.py
+++ Statistics.py
@@ -1,9 +1,9 @@
 import csv
 
-def outputStats (file_name, input_name, output_name, mask, algo, k, kmers_len, superStr_len, tm, memory, test):
+def outputStats (file_name, input_name, output_name, mask, algo, k, kmers_len, superStr_len, tm, tm1, memory, test):
 
-    header = ['Genome', 'Output File', 'Mask Type', 'Algorithm', 'K', 'Length of k-mers Set', 'Length of SuperString', 'Time', 'Memory Usage Current', 'Memory Usage Peak', 'Test Result']
-    data = [input_name, output_name, mask, algo, str(k), str(kmers_len), str(superStr_len), round(tm, 4), memory[0], memory[1], test]
+    header = ['Genome', 'Output File', 'Mask Type', 'Algorithm', 'K', 'Length of k-mers Set', 'Length of SuperString', 'CPU Time', 'Wall-clock time', 'Memory Usage Peak', 'Test Result']
+    data = [input_name, output_name, mask, algo, str(k), str(kmers_len), str(superStr_len), round(tm, 4), round(tm1, 4), memory[1], test]
 
     with open(file_name, 'w', encoding='UTF8') as f:
         writer = csv.writer(f)
