961f79b41d9c5437108c767d01b781c56533146b
Ekaterina Milyutina <65240032+Ekatmil@users.noreply.github.com>
2023-04-24 19:45:33 +0200

    Update in Automation Class: goto and fail
    Still does not work, due to the problem with number of states. The used math function does not work with larger numbers

--- Automaton_Class.py
+++ Automaton_Class.py
@@ -1,16 +1,16 @@
 #class Aho-Corasick 
-
+import math 
 class Aho_Corasick:
     def __init__ (self, kmers):
-        self.goto = {}
-        self.fail = {0:0}
+        self.goto = []
+        self.fail = []
         self.kmers = kmers
         self.goto_function()
         self.fail_function()
 
     def isLeaf(self, state):
         for char, extra in enumerate(['A', 'C', 'T','G']):
-            if self.goto[state][char] != '':
+            if self.goto[state][char] != -1:
                 return False
         return True
 
@@ -23,68 +23,40 @@ class Aho_Corasick:
 
 
     def goto_function (self):
-        num_states = len(self.kmers)
-        self.goto = [[''] * 4 for _ in range(num_states)]
+        
+        num_states = int(math.pow (len(self.kmers[0]), len(self.kmers))) #mistake. Can not work with larger numbers
+        self.goto = [[-1] * 4 for _ in range(num_states)]
         new_state = 0
-        check_count = 0
-        done = False
-
+        
         for kmer in self.kmers:
-            if done:
-                break
             state = 0
             j = 0
             for char in kmer:
                 i = ['A', 'C', 'T', 'G'].index(char)
                 res = self.goto[state][i]
-                if res == '':
+                if res == -1:
                     break
                 state = res
                 j += 1
             for char in kmer[j:]:
-                check_count = check_count + 1
-                if check_count > num_states: 
-                    done = True
-                    break
                 i = ['A', 'C', 'T', 'G'].index(char)
                 new_state += 1
                 self.goto[state][i] = new_state
-            
+                check_last = (state, i, new_state)
                 state = new_state
-     
+                split_point = new_state
 
+        self.goto = self.goto[:split_point] #to cut the list when required
 
-    # def goto_function(self):
-    #     new_state = 0
-
-    #     for kmer in self.kmers:
-    #         state = 0
-    #     #procedure enter 
-    #     # if it is new, than always FAIL and skip to third loop 
-    #         j = 0
-    #         for char in kmer:
-            
-    #             res = self.goto.get((state, char), -1)
-            
-    #             if res == -1:
-    #                 break
-    #             state = res
-    #             j += 1
-
-    #         for char in kmer[j:]:
-    #             new_state += 1
-    #             self.goto[(state, char)] = new_state #goto (starting state, value of edge) = new state
-    #             state = new_state
 
     #CONSTRUCTION OF FAIL FUNCTION (Algo 3)
     #Output: fail function in form i:j where i is current state and j is where i fails 
 
 
     def fail_function(self):
-        n = len(self.goto)
+        n = len(self.goto) + 1
         queue = []
-
-      
+        self.fail = [-1 for _ in range(n)]
 
         #store all "first" states into the queue 
         l = 0
@@ -99,7 +71,7 @@ class Aho_Corasick:
         while queue:
       
             queue_state = queue.pop(0)
-            if queue_state == len(self.goto):
+            if queue_state >= n-1:
                 continue
             
             for char, extra in enumerate(["A", "C", "T", "G"]):
@@ -107,43 +79,18 @@ class Aho_Corasick:
                 
                 res1 = self.goto[queue_state][char]
           
-                if res1 != '':
+                if res1 != -1:
                     queue.append(res1)
                     state = self.fail[queue_state]
                     while True:
-                        if state == len(self.goto):
-                            break
-              
                         res = self.goto[state][char]
-                        if res != '' or state == 0:
+                        if res != -1 or state == 0 or state == -1:
                             break
                         state = self.fail[state]
                     self.fail[res1] = res
-   
+                    
+            for i in range (len(self.fail)):
+                if self.fail[i] == -1:
+                    self.fail[i] = 0
 
-    # def fail_function(self):
-    #     queue = []
 
-    #     #store all "first" states into the queue 
-    #     l = 0
-    #     for char in ["A", "C", "T", "G"]:
-    #         res1 = self.goto.get((0,char), -1)
-    #         if res1 > 0:
-    #             queue.append(res1)
-    #             self.fail[res1] = 0
-
-    #     #construction of "inside" fails
-    #     while queue:
-    #         queue_state = queue.pop(0)
-            
-    #         for char in ["A", "C", "T", "G"]:
-    #             res1 = self.goto.get((queue_state, char), -1)
-    #             if res1 != -1:
-    #                 queue.append(res1)
-    #                 state = self.fail[queue_state]
-    #                 while True:
-    #                     res = self.goto.get((state, char), state and -1)
-    #                     if res != -1:
-    #                         break
-    #                     state = self.fail[state]
-    #                 self.fail[res1] = res
